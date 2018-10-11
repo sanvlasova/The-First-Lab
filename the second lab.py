eeae57e088c039a157e1e0451ecc8db5307154f6
sanvlasova <43313505+sanvlasova@users.noreply.github.com>
2018-10-11 23:58:26 +0300

    Update the second lab.py

--- the second lab.py
+++ the second lab.py
@@ -4,12 +4,3 @@ high_and_low("1 2 3 4 5") # return "5 1"
 high_and_low("1 2 -3 4 5") # return "5 -3"
 high_and_low("1 9 3 4 -5") # return "9 -5"
 
-nums = [1, 2, 3, 4, 5]
-print(max(nums))
-print(min(nums))
-nums = [1, 2, -3, 4, 5]
-print(max(nums))
-print(min(nums))
-nums = [1, 9, 3, 4, -5]
-print(max(nums))
-print(min(nums))
