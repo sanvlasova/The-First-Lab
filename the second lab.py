75874b78c3c90bfb858fc2285b97404fce058406
sanvlasova <43313505+sanvlasova@users.noreply.github.com>
2018-10-12 13:13:33 +0300

    Update the second lab.py

--- the second lab.py
+++ the second lab.py
@@ -4,3 +4,8 @@ high_and_low("1 2 3 4 5") # return "5 1"
 high_and_low("1 2 -3 4 5") # return "5 -3"
 high_and_low("1 9 3 4 -5") # return "9 -5"
 
+str = [int(i) for i in input().split()]
+def high_and_low(str):
+    return [hight for low in str]
+print(max(str))
+print(min(str))
