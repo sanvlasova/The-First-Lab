ef78043adf8cb8784f0b0929dad5ec910728dec6
sanvlasova <43313505+sanvlasova@users.noreply.github.com>
2018-11-15 10:19:13 +0300

    Create the second lab_function_high_and_low.py

---
+++ the second lab_function_high_and_low.py
@@ -0,0 +1,19 @@
+20. In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+Example:
+high_and_low("1 2 3 4 5") # return "5 1"
+high_and_low("1 2 -3 4 5") # return "5 -3"
+high_and_low("1 9 3 4 -5") # return "9 -5"
+
+
+def high_and_low(numbers):
+    numbers = numbers.split(" ")
+    numbers = list(map(int, numbers))
+    return max(numbers), min(numbers)
+print(high_and_low("1 2 -3 4 5"))
+
+
+def high_and_low(numbers):
+    numbers = numbers.split(' ')
+    map(int, numbers)
+    return str(max(numbers)) + ' ' + str(min(numbers))
+print(high_and_low("1 2 3 4 5"))
