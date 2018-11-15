f3a561aa2a04812ff79bbabe98660c633427921c
sanvlasova <43313505+sanvlasova@users.noreply.github.com>
2018-11-16 00:19:44 +0300

    Delete lab_3_otsenki.py

--- lab_3_otsenki.py
+++
@@ -1,9 +0,0 @@
-with open('C:/Users/Александра/Desktop/LAB_3/otsenki.txt') as f:
-    strings = [s.rstrip() for s in f.readlines()]
-otsenki = [s.split(';')[1:] for s in strings]
-for x in otsenki:
-    print(sum(map(int, x))/len(x))
-sr_mat = sum([int(x[0]) for x in otsenki]) / len(otsenki)
-sr_fiz = sum([int(x[1]) for x in otsenki]) / len(otsenki)
-sr_rus = sum([int(x[2]) for x in otsenki]) / len(otsenki)
-print(sr_mat, sr_fiz, sr_rus)
